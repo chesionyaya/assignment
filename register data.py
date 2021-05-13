52acf31430ee8249ee92424dfd5bebc84cf5751a
chesionyaya <83960173+chesionyaya@users.noreply.github.com>
2021-05-13 15:06:11 +0200

    Delete register data.py

--- register data.py
+++
@@ -1,3 +0,0 @@
-alex	lisa	123@qq.com	123123	CN	SA1	SA2	CITY1	STATE	123	Pakistan	None	123123	password1	password1
-sjad	asdasd	1234@qq.com	12312	CN1	SA3	SA4	CITY2	STATE1	123	Peru	Google	123123	password2	password2
-asdas	asdasd	123123@gmail.com	12312343	CN2	SA5	SA6	CITY3	STATE2	123	Niue	Bing	123123	password3	password3
