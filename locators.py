eed5edd7b7db9873b03ed7c657a46324fb119fba
chesionyaya <83960173+chesionyaya@users.noreply.github.com>
2021-05-13 15:05:30 +0200

    Add files via upload

--- locators.py
+++ locators.py
@@ -1,27 +1,14 @@
 from selenium.webdriver.common.by import By
-locator={
-    "login_link": (By.CLASS_NAME, "1v1-0 sign-in btn btn-white-outline open"),
-    "username":(By.id,"inputEmail"),
-    "password":(By.id,"inputPassword"),
-    "Login_button":(By.id,"login"),
-    "Register_button":(By.XPATH,'//*[@id="header"]/div/ul/li[3]/a'),
-    "fristname"(By.id,"inputFirstName"),
-    "lastname"(By.id,"inputLastName"),
-    "email"(By.id,"inputEmail"),
-    "phonenumber"(By.id,"inputPhone"),
-    "companyname"(By.id,"inputCompanyName"),
-    "address1"(By.id,"inputAddress1"),
-    "address2"(By.id,"inputAddress2"),
-    "city"(By.id,"inputCity"),
-    "state"(By.id,"stateinput"),
-    "postcode"(By.id,"inputPostcode"),
-    "country"(By.id,"inputCountry"),
-    "customfield[1]"(By.id,("customfield1"),
-    "customfield[2]"(By.id,("customfield2"),
-    "password"(By.id,("inputNewpassword1"),
-    "password"(By.id,("inputNewPassword2"),
-    "Logout_button"(By.CLASS_NAME,("btn"),
-                      
-                    
-    
-    }
+
+locator = {
+    "Registration":(By.CSS_SELECTOR,"#toggleNav > li:nth-child(6) > a"),
+    "name":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(5) > input[type=text]"),
+    "phone":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(6) > input[type=tel]"),
+    "email":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(7) > input[type=email]"),
+    "country":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(8) > select"),
+    "city":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(9) > input[type=text]"),
+    "username":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(10) > input[type=text]"),
+    "password":(By.CSS_SELECTOR,"#load_form > fieldset:nth-child(11) > input[type=password]"),
+    "submit_Button":(By.CLASS_NAME,"button")
+
+}
--- main.py
+++ main.py
@@ -8,36 +8,33 @@ import random
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(10)
 
-driver.get("https://phptravels.com/demo/")
+driver.get("http://www.way2automation.com/demo.html")
 driver.maximize_window()
 
-with open('login.csv') as csvfile:
+with open('data.csv') as csvfile:
     csvreader = reader(csvfile, delimiter=',')
     for row in csvreader:
+        assert driver.find_element(*locator["Registration"]).is_displayed()
+        driver.find_element(*locator["Registration"]).click()
 
-     assert driver.find_element(*locator["login_link"]).is_displayed
-        driver.find_element(*locator["login_link"]).click()
-        driver.find_element(*locator["username"]).send_keys(row[1])
-        driver.find_element(*locator["password"]).send_keys(row[2])
-        driver.find_element(*locator["login_button"]).click()
- with open('register.csv') as csvfile:
-    csvreader = reader(csvfile, delimiter=',')
-    for row in csvreader:                 
-       assert driver.find_element(*locator["Register_button"]).click()
-        driver.find_element(*locator["firstname"]).send_keys(row[1])
-        driver.find_element(*locator["lastname"]).send_keys(row[2])
-        driver.find_element(*locator["email"]).send_keys(str(random.randint(0,1000000))+row[0])
-        driver.find_element(*locator["phonenumber"]).send_keys(row[4])
-        driver.find_element(*locator["companynumber"]).send_keys(row[5])
-        driver.find_element(*locator["address1"]).send_keys(row[6])
-        driver.find_element(*lcoator["address2"]).send_keys(row[7])
-        driver.find_element(*lcoator["city"]).send_keys(row[8])
-        driver.find_element(*lcoator["state"]).send_keys(row[9])
-        driver.find_element(*lcoator["postcode"]).send_keys(row[10])
-        driver.find_element(*lcoator["country"]).send_keys(row[11])
-        driver.find_element(*lcoator["customfield[1]"]).send_keys(row[12])
-        driver.find_element(*lcoator["customfield[2]"]).send_keys(row[13])
-        driver.find_element(*lcoator["password"]).send_keys(row[14])
-        driver.find_element(*lcoator["password"]).send_keys(row[15])
-        driver.find_element(*lcoator["Logout_button"]).click()
+        assert driver.title == "Welcome"
+
+        driver.find_element(*locator["name"]).send_keys(row[0])
+
+        driver.find_element(*locator["phone"]).send_keys(row[1])
+
+        driver.find_element(*locator["email"]).send_keys(str(random.randint(0,1000000))+row[2])
+
+        select = Select(driver.find_element(*locator["country"]))
+        select.select_by_value(row[3])
+
+        driver.find_element(*locator["city"]).send_keys(row[4])
+
+        driver.find_element(*locator["username"]).send_keys(row[5])
+
+        driver.find_element(*locator["password"]).send_keys(row[6])
+
+        driver.find_element(*locator["submit_Button"]).click()
+
 driver.quit()
+        
